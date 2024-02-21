288ed8ded99f4a4444fde956ca89e5df20852dd0
nooseses <144011580+nooseses@users.noreply.github.com>
2024-02-21 19:32:15 +0100

    Fishing game.
    F

---
+++ Fishing game/Tiled/Tileset_water.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="Tileset_water" tilewidth="16" tileheight="16" tilecount="228" columns="19">
+ <image source="../MagicMarket/Background/Animations/Tileset_water.png" width="304" height="192"/>
+</tileset>
